9302bc965cb5512fa9102142a903fa796b6680a4
daurnimator <quae@daurnimator.com>
2017-12-06 14:08:39 +1100

    src/lualdap.c: Use lua_getfield in get_attrs_param

--- src/lualdap.c
+++ src/lualdap.c
@@ -808,12 +808,11 @@ static void create_search (lua_State *L, int conn_index, int msgid) {
 ** Fill in the attrs array, according to the attrs parameter.
 */
 static int get_attrs_param (lua_State *L, char *attrs[]) {
-	lua_pushstring (L, "attrs");
-	lua_gettable (L, 2);
-	if (lua_isstring (L, -1)) {
+	int type = lua_getfield(L, 2, "attrs");
+	if (type == LUA_TSTRING) {
 		attrs[0] = (char *)lua_tostring (L, -1);
 		attrs[1] = NULL;
-	} else if (!lua_istable (L, -1))
+	} else if (type != LUA_TTABLE)
 		attrs[0] = NULL;
 	else
 		if (table2strarray (L, lua_gettop (L), attrs, LUALDAP_MAX_ATTRS))
